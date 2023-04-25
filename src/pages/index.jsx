f32c4f7cf0b22e6336eef94daa2bc5e5ecf26639
sofaemha <mail@sofa.my.id>
2023-04-25 16:08:24 +0700

    create dummy content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -34,8 +34,42 @@ export default function Index() {
 			
       <Container className="mt-5">
         <div className="grid max-w-xl gap-4 sm:grid-cols-2 lg:max-w-none">
-					{
-						data.map((item) => (
+					{ (data)
+						? (
+							<>
+								<div key="dummy1" className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700 animate-pulse">
+									<div className="mb-2 h-2.5 w-48 rounded-full bg-white/90"></div>
+									<p className="mb-3">
+										<div className="mb-2.5 h-2 w-full max-w-[360px] rounded-full bg-gray-400/90"></div>
+										<div className="mb-2.5 h-2 w-[80%] rounded-full bg-gray-400/90"></div>
+										<div className="mb-2.5 h-2 w-[50%] max-w-[330px] rounded-full bg-gray-400/90"></div>
+									</p>
+									
+									<a href={`https://github.sofa.my.id/${item.name}`} className="inline-flex py-2 px-5 mr-2 border border-zinc-700 rounded-md bg-zinc-700 items-center">
+										<div className="h-2 w-48 rounded-full bg-white"></div>
+									</a>
+									<a href={`https://github.com/sofaemha/${item.name}`} className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center">
+										<div className="h-2 w-48 rounded-full bg-white"></div>
+									</a>
+								</div>
+								<div key="dummy2" className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700 animate-pulse">
+									<div className="mb-2 h-2.5 w-48 rounded-full bg-white/90"></div>
+									<p className="mb-3">
+										<div className="mb-2.5 h-2 w-full max-w-[360px] rounded-full bg-gray-400/90"></div>
+										<div className="mb-2.5 h-2 w-[80%] rounded-full bg-gray-400/90"></div>
+										<div className="mb-2.5 h-2 w-[50%] max-w-[330px] rounded-full bg-gray-400/90"></div>
+									</p>
+									
+									<a href={`https://github.sofa.my.id/${item.name}`} className="inline-flex py-2 px-5 mr-2 border border-zinc-700 rounded-md bg-zinc-700 items-center">
+										<div className="h-2 w-48 rounded-full bg-white"></div>
+									</a>
+									<a href={`https://github.com/sofaemha/${item.name}`} className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center">
+										<div className="h-2 w-48 rounded-full bg-white"></div>
+									</a>
+								</div>
+							</>
+						)
+						: data.map((item) => (
 							<div key={item.id} className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700">
 								<h5 className="mb-2 text-2xl font-semibold tracking-tight text-white">{item.name}</h5>
 								<p className="mb-3 font-normal text-gray-400">{item.description}</p>
