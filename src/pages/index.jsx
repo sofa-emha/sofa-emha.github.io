99730827fdd882b6b2250ca66934e1cbe214b730
sofaemha <mail@sofa.my.id>
2023-04-26 19:18:12 +0700

    change anchor behavior to "_self"

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -38,29 +38,22 @@ export default function Index() {
         <div className="grid max-w-xl gap-4 sm:grid-cols-2 lg:max-w-none">
 					{ (loading) ? (
 							<>
-								<div key="dummy1" className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700 animate-pulse">
-									<div className="mb-2 h-2.5 w-48 rounded-full bg-white/90"></div>
-									<p className="mb-3">
-										<div className="mb-2.5 h-2 w-full max-w-[360px] rounded-full bg-gray-400/90"></div>
-										<div className="mb-2.5 h-2 w-[80%] rounded-full bg-gray-400/90"></div>
-										<div className="mb-2.5 h-2 w-[50%] max-w-[330px] rounded-full bg-gray-400/90"></div>
-									</p>
-									
-									<a className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center">
-										<div className="h-2 w-48 rounded-full bg-white"></div>
-									</a>
-								</div>
-								<div key="dummy2" className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700 animate-pulse">
-									<div className="mb-2 h-2.5 w-48 rounded-full bg-white/90"></div>
-									<p className="mb-3">
-										<div className="mb-2.5 h-2 w-full max-w-[360px] rounded-full bg-gray-400/90"></div>
-										<div className="mb-2.5 h-2 w-[80%] rounded-full bg-gray-400/90"></div>
-										<div className="mb-2.5 h-2 w-[50%] max-w-[330px] rounded-full bg-gray-400/90"></div>
-									</p>
-									<a className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center">
-										<div className="h-2 w-48 rounded-full bg-white"></div>
-									</a>
-								</div>
+								{
+									Array(6).map(index => (
+										<div key={`dummy${index}`} className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700 animate-pulse">
+											<div className="mb-2 h-2.5 w-48 rounded-full bg-zinc-100"></div>
+											<p className="mb-3">
+												<div className="mb-2.5 h-2 w-full max-w-[360px] rounded-full bg-gray-400/90"></div>
+												<div className="mb-2.5 h-2 w-[80%] rounded-full bg-gray-400/90"></div>
+												<div className="mb-2.5 h-2 w-[50%] max-w-[330px] rounded-full bg-gray-400/90"></div>
+											</p>
+											
+											<a className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center">
+												<div className="h-2 w-48 rounded-full bg-zinc-100"></div>
+											</a>
+										</div>
+									))
+								}
 							</>
 						) : (
 							data.map((item) => (
@@ -69,12 +62,12 @@ export default function Index() {
 									<p className="mb-3 font-normal text-gray-400">{item.description}</p>
 									{
 										item.pages && (
-											<a href={`https://github.sofa.my.id/${item.name}`} className="inline-flex py-2 px-5 mr-2 border border-zinc-700 rounded-md bg-zinc-700 items-center text-white">
+											<a href={`https://github.sofa.my.id/${item.name}`} target="_self" className="inline-flex py-2 px-5 mr-2 border border-zinc-700 rounded-md bg-zinc-700 items-center text-white">
 												Preview
 											</a>
 										)
 									}
-									<a href={`https://github.com/sofaemha/${item.name}`} className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center text-white">
+									<a href={`https://github.com/sofaemha/${item.name}`} target="_self" className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center text-white">
 										Repository
 									</a>
 								</div>
