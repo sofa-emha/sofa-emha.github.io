308cefba6deaadc49d0a225a336afe279b5989d6
sofaemha <mail@sofa.my.id>
2023-04-26 19:29:12 +0700

    create Skeleton

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,28 @@ import { Octokit } from "@octokit/core";
 import { useState, useEffect } from "react";
 import { Container } from "@/components/Container";
 
+function Skeleton() {
+	return (
+		<>
+			{
+				Array(6).fill(0).map(index => (
+					<div key={`dummy${index}`} className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700 animate-pulse">
+						<div className="mb-2 h-2.5 w-48 rounded-full bg-zinc-100"></div>
+						<p className="mb-3">
+							<div className="mb-2.5 h-2 w-full max-w-[360px] rounded-full bg-gray-400/90"></div>
+							<div className="mb-2.5 h-2 w-[80%] rounded-full bg-gray-400/90"></div>
+							<div className="mb-2.5 h-2 w-[50%] max-w-[330px] rounded-full bg-gray-400/90"></div>
+						</p>
+						
+						<a className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center">
+							<div className="h-2 w-48 rounded-full bg-zinc-100"></div>
+						</a>
+					</div>
+				))
+			}
+		</>
+	)
+}
 async function Content() {
 	let resource = []
 	const octokit = new Octokit({ auth: process.env.GITHUB_AUTH_TOKEN })
@@ -38,22 +60,7 @@ export default function Index() {
         <div className="grid max-w-xl gap-4 sm:grid-cols-2 lg:max-w-none">
 					{ (loading) ? (
 							<>
-								{
-									Array(6).fill(0).map(index => (
-										<div key={`dummy${index}`} className="p-6 rounded-lg shadow bg-zinc-800 border-zinc-700 animate-pulse">
-											<div className="mb-2 h-2.5 w-48 rounded-full bg-zinc-100"></div>
-											<p className="mb-3">
-												<div className="mb-2.5 h-2 w-full max-w-[360px] rounded-full bg-gray-400/90"></div>
-												<div className="mb-2.5 h-2 w-[80%] rounded-full bg-gray-400/90"></div>
-												<div className="mb-2.5 h-2 w-[50%] max-w-[330px] rounded-full bg-gray-400/90"></div>
-											</p>
-											
-											<a className="inline-flex py-2 px-5 border border-zinc-700 rounded-md bg-zinc-700 items-center">
-												<div className="h-2 w-48 rounded-full bg-zinc-100"></div>
-											</a>
-										</div>
-									))
-								}
+								<Skeleton/>
 							</>
 						) : (
 							data.map((item) => (
